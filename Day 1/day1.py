383f3da795355aae458d0441e012df85456199ee
Qhotti <kody.pudney@gmail.com>
2022-07-25 15:09:41 -0700

    day 1

---
+++ Day 1/day1.py
@@ -0,0 +1,16 @@
+#cool input thing
+#print("Hello " + input("what is your name"))
+
+
+#prints the number of characters in the inputted name
+#print(len(input('What is your name? ') ))
+
+#variable test
+#user_name = input('Name? ')
+#print('Hello ' + user_name + '!')
+
+print('Welcome to the Band Name Generator!')
+city = input('What is the name of the city you grew up in? ')
+pet = input('What is the name of your pet? ')
+
+print("Band Name: " + city + " " + + pet)
