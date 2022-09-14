c1dff31c9f745bc2f304290d0ad8d6d84091f45b
Qhotti <kody.pudney@gmail.com>
2022-09-14 16:54:25 -0700

    day 25

--- Day25/day25.py
+++ Day25/day25.py
@@ -2,7 +2,8 @@
 #    data = data_file.readlines()
 #    print(data)
 import csv
-
+from optparse import TitledHelpFormatter
+import pandas
 # with open('Day25\weather-data.csv') as data_file:
 #     data = csv.reader(data_file)
 #     temperatures = []
@@ -11,7 +12,7 @@ import csv
 #             temperatures.append(int(row[1]))
 #     print(temperatures)
 
-import pandas
+
 
 #data = pandas.read_csv('Day25\weather-data.csv')
 #print(data['temp'])                                # good way of getting one row of data in csv using pandas
@@ -45,18 +46,74 @@ import pandas
 #data = pandas.DataFrame(data_dict)
 #data.to_csv("new_data.csv")
 
+#squirrel thing
+#data = pandas.read_csv('Day25\Central-Park-Squirrel-Census-Squirrel-Data.csv')
+#grey_squirrels_count = len(data[data['Primary Fur Color'] == 'Gray'])
+#red_squirrels_count = len(data[data['Primary Fur Color'] == 'Cinnamon'])
+#black_squirrels_count = len(data[data['Primary Fur Color'] == 'Black'])
+#print(grey_squirrels_count)
+#print(red_squirrels_count)
+#print(black_squirrels_count)
+#
+#data_dict = {
+#    'Fur Color': ["Gray", 'Red', 'Black'],
+#    'Count': [grey_squirrels_count, red_squirrels_count, black_squirrels_count]
+#}
+#df = pandas.DataFrame(data_dict)
+#df.to_csv("squirrel_data.csv")
 
-data = pandas.read_csv('Day25\Central-Park-Squirrel-Census-Squirrel-Data.csv')
-grey_squirrels_count = len(data[data['Primary Fur Color'] == 'Gray'])
-red_squirrels_count = len(data[data['Primary Fur Color'] == 'Cinnamon'])
-black_squirrels_count = len(data[data['Primary Fur Color'] == 'Black'])
-print(grey_squirrels_count)
-print(red_squirrels_count)
-print(black_squirrels_count)
 
-data_dict = {
-    'Fur Color': ["Gray", 'Red', 'Black'],
-    'Count': [grey_squirrels_count, red_squirrels_count, black_squirrels_count]
-}
-df = pandas.DataFrame(data_dict)
-df.to_csv("squirrel_data.csv")
+
+#U.S. Game
+
+import turtle 
+
+screen = turtle.Screen()
+screen.title('U.S. Game')
+image = 'blank_states_img.gif'
+screen.addshape(image)
+turtle.shape(image)
+game_is_on = True
+states_csv = pandas.read_csv('50_states.csv')
+score = 0
+state_dict = states_csv.to_dict()
+
+
+while game_is_on:
+    answer = screen.textinput(title=f'Score: {score}/50 ', prompt='Enter State Name.\nend to exit.')
+
+    def new_state():
+        turtle.hideturtle()
+        turtle.penup() 
+        turtle.color('black')
+        turtle.goto(x,y)
+        turtle.write(state)
+
+
+    if answer in state_dict['state'].values():
+        score += 1
+        state = answer
+        number = state_dict['state']
+        things = state_dict['state'][answer]
+        #x = state_dict['x'].get[]
+        #y =
+        #new_state()
+    
+    if answer == 'end':
+        exit()
+    print(things)
+
+
+
+
+
+
+
+
+
+
+
+
+
+turtle.exitonclick()
+
