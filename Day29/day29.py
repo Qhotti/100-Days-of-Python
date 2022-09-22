3cbfb21d4b796fae2928d34bc6b5f175c0149264
Qhotti <kody.pudney@gmail.com>
2022-09-21 18:33:39 -0700

    fixes for day29

--- Day29/day29.py
+++ Day29/day29.py
@@ -1,40 +1,28 @@
 from tkinter import *
 from tkinter import messagebox
-import random
+from random import choice, randint, shuffle
 import pyperclip
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 #Password Generator Project
 def generate_password():
-  password_input.delete(0,END)
-  letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-  numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-  symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+    password_input.delete(0,END)
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-  nr_letters = random.randint(8, 10)
-  nr_symbols = random.randint(2, 4)
-  nr_numbers = random.randint(2, 4)
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
 
-  password_list = []
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
 
-  for char in range(nr_letters):
-    password_list.append(random.choice(letters))
+    password = "".join(password_list)
+    password_input.insert(0, password)
+    pyperclip.copy(password)
 
-  for char in range(nr_symbols):
-    password_list += random.choice(symbols)
-
-  for char in range(nr_numbers):
-    password_list += random.choice(numbers)
-
-  random.shuffle(password_list)
-
-  password = ""
-  for char in password_list:
-    password += char
-    
-  password_input.insert(0, f'{password}')
-  pyperclip.copy(f'{password}')
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
