454506a193480658ac051e851db4a2ad724549b8
Qhotti <kody.pudney@gmail.com>
2022-10-26 23:22:43 -0700

    day54

--- Day54/app.py
+++ Day54/app.py
@@ -1,15 +1,15 @@
-import time
-from flask import Flask
-app=Flask(__name__)
+# import time
+# from flask import Flask
+# app=Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello, World!</p>"
 
-@app.route("/bye") #when client goes to website/bye 'say goodbye'
-def say_bye():
-    return "<p>Goodbye</p>"
+# @app.route("/bye") #when client goes to website/bye 'say goodbye'
+# def say_bye():
+#     return "<p>Goodbye</p>"
 
 # def delay_decorator(function):   #function is say_hello()
 #     def wrapper_function():
@@ -22,4 +22,30 @@ def say_bye():
 # @delay_decorator  #basically is decorated_function = delay_decorator(say_hello)
 # def say_hello():
 #     print('hello')
-    
+
+
+#challenge 1
+# import time
+# current_time = time.time()
+
+# def speed_calc_decorator(function):
+#     def thing():
+#         time1 = time.time()
+#         function()
+#         time2= time.time()
+#         tt = time2 - time1
+#         print(f'{function.__name__} run speed: {tt}')
+#     return thing
+
+# @speed_calc_decorator
+# def fast_function():
+#     for i in range(10000000):
+#         i * i
+
+# @speed_calc_decorator
+# def slow_function():
+#     for i in range(100000000):
+#         i * i
+
+# fast_function()
+# slow_function()
